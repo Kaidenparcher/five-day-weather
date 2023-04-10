a77736abaee84102bd59ea7fd6f5a976aa6f30f3
Kaiden Parcher <ktparcher@.com>
2023-04-09 18:02:34 -0700

    add in begining varriables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
+var userFormEl = document.querySelector('#city-search');
+var nameInputEl = document.querySelector('#current-weather');
+var repoContainerEl = document.querySelector('#wather-container');
+var weatherResults = document.querySelector('#weather-results');
+
+
+
+
+
+
+
+
+
+
+
+
+
 // e7061ed9c868477223ac6802888315d2
 
 
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
