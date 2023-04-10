ee59025f0d019a167737bbdb95542825d36f1f6d
Kaiden Parcher <ktparcher@.com>
2023-04-09 18:07:14 -0700

    add weather url to java and built function to fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,24 @@ var repoContainerEl = document.querySelector('#wather-container');
 var weatherResults = document.querySelector('#weather-results');
 
 
+var getCurrentWeather = function (weather) {
+    // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+    var apiUrl = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={e7061ed9c868477223ac6802888315d2};
+  
+    fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            displayRepos(data, weather);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+  };
 
 
 
@@ -14,10 +32,9 @@ var weatherResults = document.querySelector('#weather-results');
 
 
 
+e7061ed9c868477223ac6802888315d2
 
-// e7061ed9c868477223ac6802888315d2
 
-
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
