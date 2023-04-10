d7044ec4cb98fd0e5967c06f1344d04d7c72e0f0
Kaiden Parcher <ktparcher@.com>
2023-04-09 17:43:48 -0700

    added some styling and also links to openweather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
 // e7061ed9c868477223ac6802888315d2
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
